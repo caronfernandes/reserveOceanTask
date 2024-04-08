121da71c54a586289edb7ff14bd09b2a2c405707
caronfernandes <caron@ic4.health>
2024-04-08 14:54:28 +0530

    db config for host

--- server/connect.js
+++ server/connect.js
@@ -1,8 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hilton-hotel",
+  host: "b89bdamdpomdgqnnqkrl-mysql.services.clever-cloud.com",
+  user: "urzbtt5fekhoazu2",
+  password: "b89bdamdpomdgqnnqkrl",
+  database: "b89bdamdpomdgqnnqkrl",
 });
